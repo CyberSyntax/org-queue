63210368091338ea0462c33c0b2df57e4ee94265
CyberSyntax <137813366+CyberSyntax@users.noreply.github.com>
2025-09-09 09:21:34 +0900

    Change global keybinding for org-queue prefix
    The global keybinding for org-queue-prefix-map was updated from C-; to C-c q to avoid conflicts and improve usability.

--- org-queue.el
+++ org-queue.el
@@ -60,8 +60,8 @@
   (define-key org-queue-prefix-map (kbd "z") #'org-interactive-cloze)
   (define-key org-queue-prefix-map (kbd "Z") #'org-queue-srs-item-create-card))
 
-;; Bind the prefix globally to C-;
-(global-set-key (kbd "C-;") org-queue-prefix-map)
+;; Bind the prefix globally to C-c q
+(global-set-key (kbd "C-c q") org-queue-prefix-map)
 
 (provide 'org-queue)
 ;;; org-queue.el ends here
